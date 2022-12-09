43ea3fc3a2ede2686ad9373b7b36fa6f1494ac47
Manuel Atance <ryuusei3@MacBook-Pro-de-Manuel.local>
2022-12-09 18:58:19 +0100

    Fixed image update on Notes and Kanban

--- routes/kanban.routes.js
+++ routes/kanban.routes.js
@@ -1,10 +1,8 @@
 const router = require("express").Router()
 
-const { getKanbanByUserId,
-    getAllKanban,
-    updateKanban,
-    deleteKanban,
-    createKanban } = require('../controllers/kanban.controller')
+const Kanban = require("./../models/Kanban.model")
+
+const { getKanbanByUserId, getAllKanban, updateKanban, deleteKanban, createKanban } = require("../controllers/kanban.controller")
 
 const { isAuthenticated } = require("../middleware/jwt.middleware")
 
@@ -13,6 +11,19 @@ router.post("/new", isAuthenticated, createKanban)
 router.get("/", isAuthenticated, getKanbanByUserId)
 
 router.put("/update/:id", updateKanban)
+
+router.put("/update/image/:id", (req, res, next) => {
+	const { id: kanban_id } = req.params
+	const { image } = req.body
+
+	const header = {
+		image: image,
+	}
+
+	Kanban.findByIdAndUpdate(kanban_id, { header }, { new: true })
+		.then(response => res.json(response))
+		.catch(err => next(err))
+})
 
 router.delete("/delete/:id", isAuthenticated, deleteKanban)
 
--- routes/notes.routes.js
+++ routes/notes.routes.js
@@ -1,12 +1,8 @@
 const router = require("express").Router()
 
-const { createNote,
-    getNote,
-    updateNote,
-    getNotesByUserId,
-    deleteNote,
-    deleteAllNotes,
-    shareNote } = require('./../controllers/notes.controller')
+const Notes = require("./../models/Notes.model")
+
+const { createNote, getNote, updateNote, getNotesByUserId, deleteNote, deleteAllNotes, shareNote } = require("./../controllers/notes.controller")
 
 const { isAuthenticated } = require("./../middleware/jwt.middleware")
 
@@ -17,6 +13,19 @@ router.get("/shared", shareNote)
 router.get("/:id", getNote)
 
 router.put("/update/:id", updateNote)
+
+router.put("/update/image/:id", (req, res, next) => {
+	const { id: notes_id } = req.params
+	const { image } = req.body
+
+	const header = {
+		image: image,
+	}
+
+	Notes.findByIdAndUpdate(notes_id, { header }, { new: true })
+		.then(response => res.json(response))
+		.catch(err => next(err))
+})
 
 router.delete("/deletemany/:id", isAuthenticated, deleteAllNotes)
 
