f6a05b564ea9861667f5c909f6c296a8c3bb86a3
Manuel Atance <ryuusei3@MacBook-Pro-de-Manuel.local>
2022-12-12 13:56:08 +0100

    Added new routes for Single notes

--- routes/singleNote.routes.js
+++ routes/singleNote.routes.js
@@ -1,16 +1,18 @@
 const router = require("express").Router()
 
-const { createNote, deleteNote, getNote, getNoteListByUser } = require("../controllers/singleNote.controllers")
-/* const { isAuthenticated } = require("../middleware/jwt.middleware") */
+const { createNote, deleteNote, getNote, updateSingleNoteHeader, getNoteListByUser } = require("../controllers/singleNote.controllers")
+const { isAuthenticated } = require("../middleware/jwt.middleware")
 
 /* router.put("/update/:id", updateNote) */
 
 router.get("/:id", getNote)
 
-router.get("/list/:id", getNoteListByUser)
+router.get("/list/:id", isAuthenticated, getNoteListByUser)
 
 router.delete("/delete/:id", deleteNote)
 
-router.post("/new", createNote)
+router.put("/update/header/:id", updateSingleNoteHeader)
+
+router.post("/new", isAuthenticated, createNote)
 
 module.exports = router
