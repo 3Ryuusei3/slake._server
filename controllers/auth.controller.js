d152a4d64941204fc7e17f91426799bac86ca11c
Manuel Atance <ryuusei3@MacBook-Pro-de-Manuel.local>
2022-12-15 09:33:01 +0100

    Fixed issue on signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,89 +4,75 @@ const Kanban = require("./../models/Kanban.model")
 const Note = require("./../models/Notes.model")
 const SingleNoteModel = require("../models/SingleNote.model")
 
-
-
 const signup = async (req, res, next) => {
+	try {
+		const { email, password, username, imageUrl } = req.body
 
-    try {
-        const { email, password, username, imageUrl } = req.body
+		const createdUser = await User.create({ email, password, username, imageUrl })
 
-        const createdUser = await User.create({ email, password, username, imageUrl })
+		const { _id } = createdUser
+		const user = { email, username, _id, imageUrl }
 
-        const { _id } = createdUser
-        const user = { email, username, _id, imageUrl, isDark }
+		const createDashboard = await Dashboard.create({ owner: createdUser._id })
+		const createKanban = await Kanban.create({ owner: createdUser._id })
+		const createNoteList = await Note.create({ owner: createdUser._id })
+		const createNote = await SingleNoteModel.create({ owner: createdUser._id })
 
-        const createDashboard = await Dashboard.create({ owner: createdUser._id })
-        const createKanban = await Kanban.create({ owner: createdUser._id })
-        const createNoteList = await Note.create({ owner: createdUser._id })
-        const createNote = await SingleNoteModel.create({ owner: createdUser._id })
-
-        res.status(201).json({ user, createDashboard, createKanban, createNoteList, createNote })
-
-    } catch (err) {
-        next(err)
-    }
+		res.status(201).json({ user, createDashboard, createKanban, createNoteList, createNote })
+	} catch (err) {
+		next(err)
+	}
 }
 
 const login = (req, res, next) => {
+	const { email, password } = req.body
 
-    const { email, password } = req.body
+	if (email === "" || password === "") {
+		res.status(400).json({ message: "Provide email or password" })
+		return
+	}
 
-    if (email === "" || password === "") {
-        res.status(400).json({ message: "Provide email or password" })
-        return
-    }
-
-    User.findOne({ email })
-        .then(foundUser => {
-            if (foundUser && foundUser.validatePassword(password)) {
-                res.status(200).json({ authToken: foundUser.signToken() })
-            }
-            else {
-                res.status(401).json({ message: "Unable to authenticate the user" })
-            }
-        })
-        .catch(err => next(err))
+	User.findOne({ email })
+		.then(foundUser => {
+			if (foundUser && foundUser.validatePassword(password)) {
+				res.status(200).json({ authToken: foundUser.signToken() })
+			} else {
+				res.status(401).json({ message: "Unable to authenticate the user" })
+			}
+		})
+		.catch(err => next(err))
 }
-
 
 const verify = (req, res) => {
-    res.status(200).json(req.payload)
+	res.status(200).json(req.payload)
 }
 
-
 const refreshToken = (req, res, next) => {
-
-    User.findById(req.payload._id)
-        .then((newToken) => {
-            res.status(200).json({ refreshedToken: newToken.signToken() })
-        })
-        .catch(err => next(err))
+	User.findById(req.payload._id)
+		.then(newToken => {
+			res.status(200).json({ refreshedToken: newToken.signToken() })
+		})
+		.catch(err => next(err))
 }
 
 const deleteUser = async (req, res, next) => {
+	try {
+		const deleteUser = await User.findByIdAndRemove(req.payload._id)
 
-    try {
+		const deleteDashboard = await Dashboard.deleteOne({ owner: req.payload._id })
+		const deleteKanban = await Kanban.deleteOne({ owner: req.payload._id })
+		const deleteNote = await Note.deleteOne({ owner: req.payload._id })
 
-        const deleteUser = await User.findByIdAndRemove(req.payload._id)
-
-        const deleteDashboard = await Dashboard.deleteOne({ owner: req.payload._id })
-        const deleteKanban = await Kanban.deleteOne({ owner: req.payload._id })
-        const deleteNote = await Note.deleteOne({ owner: req.payload._id })
-
-        res.status(201)
-
-    }
-    catch (err) {
-        next(err)
-    }
-
+		res.status(201)
+	} catch (err) {
+		next(err)
+	}
 }
 
 module.exports = {
-    signup,
-    login,
-    verify,
-    refreshToken,
-    deleteUser
+	signup,
+	login,
+	verify,
+	refreshToken,
+	deleteUser,
 }
--- error-handling/index.js
+++ error-handling/index.js
@@ -1,22 +1,22 @@
-module.exports = (app) => {
+module.exports = app => {
+	app.use((req, res, next) => {
+		res.status(404).json({ message: "This route does not exist" })
+	})
 
-  app.use((req, res, next) => {
-    res.status(404).json({ message: "This route does not exist" })
-  })
+	app.use((err, req, res, next) => {
+		if (err.code && err.code === 11000) {
+			res.status(409).json({ errorMessages: ["This email is already in the database"] })
+		}
 
-  app.use((err, req, res, next) => {
+		if (err.name === "ValidationError") {
+			let errorMessages = Object.values(err.errors).map(el => el.message)
+			res.status(400).json({ errorMessages })
+		}
 
-    if (err.code && err.code === 11000) {
-      res.status(409).json({ errorMessages: ['This email is already in the database'] })
-    }
+		console.log("Server error: ", err)
 
-    if (err.name === 'ValidationError') {
-      let errorMessages = Object.values(err.errors).map(el => el.message)
-      res.status(400).json({ errorMessages })
-    }
-
-    if (!res.headersSent) {
-      res.status(500).json({ errorMessages: ["Internal server error. Check the server console"] })
-    }
-  })
+		if (!res.headersSent) {
+			res.status(500).json({ errorMessages: ["Internal server error. Check the server console"] })
+		}
+	})
 }
