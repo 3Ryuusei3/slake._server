727413834e11dfe3f32e9cbeb0d436b0f424749f
Manuel Atance <atance3@hotmail.com>
2023-01-09 11:44:08 +0100

    Small cleanup

--- routes/calendar.routes.js
+++ routes/calendar.routes.js
@@ -1,21 +1,12 @@
+const router = require("express").Router()
+const { isAuthenticated } = require("../middleware/jwt.middleware")
 
-const router = require('express').Router()
-const { isAuthenticated } = require('../middleware/jwt.middleware')
+const { getCalendarByUserID, newCalendar, deleteCalendar } = require("../controllers/calendar.controller")
 
-const { getCalendarByUserID, newCalendar, deleteCalendar } = require('../controllers/calendar.controller')
+router.get("/", isAuthenticated, getCalendarByUserID)
 
-
-
-router.get('/', isAuthenticated, getCalendarByUserID)
-
-router.post('/new', isAuthenticated, newCalendar)
+router.post("/new", isAuthenticated, newCalendar)
 
 router.delete("/delete/:id", isAuthenticated, deleteCalendar)
-
-
-
-
-
-
 
 module.exports = router
